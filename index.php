c2f97e90a6a5ce7a9ee4ed11c673ebe95c592cb7
ivan-junior <52333270+ivan-junior@users.noreply.github.com>
2019-08-26 10:39:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,37 +4,21 @@ require_once(__DIR__ . '../../../config.php');
 require_once('lib/locallib.php');
 require_login();
 
-/*if ($USER->id = 81) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-}*/
-
-$title = "Página de Teste";
-$pagetitle = "Página de Teste";
-$url = new moodle_url("/local/bluesolmy/index.php");
+$title = "My Moodle Progress";
+$pagetitle = "My Moodle Progress";
+$url = new moodle_url("/local/mymoodleprogress/index.php");
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url($url);
 $PAGE->set_title($title);
 $PAGE->set_heading($title);
 $PAGE->set_pagelayout('standart');
-$PAGE->requires->css('/local/bluesolmy/style/style.css');
+$PAGE->requires->css('/local/mymoodleprogress/style/style.css');
 echo $OUTPUT->header();
 
-
-echo "<h2>Testando</h2>";
 $data = get_all_information($USER->id);
 echo "<div class='row topo'>";
 foreach ($data as $id => $course) {
-	//echo "ID Curso: " . $course['id'] . " Nome do curso: " . $course['fullname'] . " Concluído em: " . date('Y', $course['timecompleted']) . " Progresso: " . $course['progress'] . "% <br />";
 	echo make_data($course['id'], $course['fullname'], $course['timecompleted'], $course['progress']);
 }
-echo "</div>";
-//print_r($USER);
-/*if (!empty($_SESSION['USER']->realuser)) {
-    // Logged in as.
-    $realuserid = $_SESSION['USER']->id;
-    echo $realuserid;
-}*/
 
 echo $OUTPUT->footer();
