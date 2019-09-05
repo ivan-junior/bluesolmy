73a75d8108fb53a5c8c73f2597e2e2e81e34373a
ivan-junior <52333270+ivan-junior@users.noreply.github.com>
2019-09-05 16:37:10 -0300

    Update version.php

--- version.php
+++ version.php
@@ -4,4 +4,4 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2019082100;         // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018051700;         // Requires this Moodle version for Bootstrap 4
 $plugin->component = 'local_mymoodleprogress'; // Full name of the plugin (used for diagnostics)
-$plugin->release = '1.0.2';
+$plugin->release = '1.0.4';
